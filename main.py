0151ba90d70c1acffeedc5854303778b10901a9e
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-04 16:40:56 +0545

    .

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-number=int(input("Enter a number to check palindrome or not:"))
+# number=int(input("Enter a number to check palindrome or not:"))
 
-originalno=number
-reverse=0
+# originalno=number
+# reverse=0
 
-while number>0:
-    digit=number%10
-    reverse=reverse*10+digit
-    number//=10
+# while number>0:
+#     digit=number%10
+#     reverse=reverse*10+digit
+#     number//=10
     
-if originalno==reverse:
-    print("Number is palindrome")
-else:
-    print("Number is not palindrome")
+# if originalno==reverse:
+#     print("Number is palindrome")
+# else:
+#     print("Number is not palindrome")
+
+
+nol=int(input("Enter the largest number:"))
+nos=int(input("Enter the smallest number:"))
+
+while nos:
+    nostore=nos
+    nos=nol%nos
+    nol=nostore
+    
+print("HCF is:",nol)
